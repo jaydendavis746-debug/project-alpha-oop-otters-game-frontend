ee6882a34f50d266ab22359f68ca36105d6fd511
unknown <ismaeelhaque@gmail.com>
2026-09-02 10:28:33 +0100

    Added HTML structure for index.html, now onto styling

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 const loginForm = document.getElementById('login-form');
-const loginMessage = document.getElementById;
+const loginMessage = document.getElementById('login-message');
 
 loginForm.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -7,11 +7,28 @@ loginForm.addEventListener('submit', function(event) {
     const inputUserName = document.getElementById('login-username').value;
     const inputPassword = document.getElementById('login-password').value
 
-    const userBase = JSON.parse(localStorage.getItem('fakeUser')) || [];
+    const userBase = JSON.parse(localStorage.getItem('fakeUsers')) || [];
 
     const userFound = userBase.find(user => user.username === inputUserName)
 
     if (!userFound) {
-        
+        loginMessage.textContent = "Username not found. Please sign up.";
+        return;
     }
+
+    if (userFound.password !== inputPassword) {
+        loginMessage.textContent = "Incorrect Password."
+        return;
+    }
+
+    loginMessage.textContent = "Login Success"
+
+    localStorage.setItem('currentUser', JSON.stringify ({
+        username: userFound.username
+    }));
+
+    console.log("successful")
+    setTimeout(() => {
+    window.location.href = '/pages/subjects.html';
+    }, 1000);
 })
