ae9eba69310c79fe74c2e7d2515ff30eef851028
jaydendavis746-debug <jaydendavis746@gmail.com>
2026-09-02 11:03:02 +0100

    Got teh quiz logic tyo work with teh mock data and shoes c=score in teh local storage also protected route

--- js/results.js
+++ js/results.js
@@ -1,1 +1,8 @@
-import { result } from '../mock/results';
+function protectRoute(){
+    const session = JSON.parse(localStorage.getItem("session"))
+    if(!session || session.loggedIn){
+        window.location.href = 'login.html'   
+    }
+}
+
+protectRoute()
